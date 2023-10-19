8415c299d3fe723f47ba9911b7dec3cc8da43901
AIE\s229025 <jrverburg@yahoo.com>
2023-10-19 14:13:37 -0700

    Movement System

---
+++ GyroPlatFormer/Assets/Scripts/ButtonFix.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonFix : MonoBehaviour
+{
+    public Color Highlighted;
+    public Color NormalColor;
+    private Image Button;
+    public Type ButtonType;
+    public bool isholding;
+    private void Start()
+    {
+        Button = gameObject.GetComponent<Image>();
+    }
+    public void Update()
+    {
+        if (isholding)
+        {
+            Button.color = Highlighted;
+            if (ButtonType == Type.Forward)
+            {
+                MoveMent.moveforward();
+            }
+            if (ButtonType == Type.Backward)
+            {
+                MoveMent.moveBack();
+            }
+            if (ButtonType == Type.Left)
+            {
+                MoveMent.moveLeft();
+            }
+            if (ButtonType == Type.Right)
+            {
+                MoveMent.moveRight();
+            }
+        }
+        else
+        {
+            Button.color = NormalColor;
+        }
+    }
+    public void Buttonhold(bool Select)
+    {
+        isholding = Select;
+        return;
+    }
+
+    public enum Type
+    {
+        Forward,
+        Backward,
+        Left,
+        Right
+    }
+
+}
