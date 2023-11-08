e98e1874808dff78c65efe40cf848fa6188ef308
AIE\s229025 <jrverburg@yahoo.com>
2023-11-08 15:14:40 -0800

    NI

--- GyroPlatFormer/Assets/Scripts/MoveMent.cs
+++ GyroPlatFormer/Assets/Scripts/MoveMent.cs
@@ -48,7 +48,8 @@ public class MoveMent : MonoBehaviour
         MoveDiection = GameObject.Find("PlayerHead");
         if (IsMoving == false && Inair == false)
         {
-            PlayerRB.AddForce(new Vector3(0,0,0), ForceMode.VelocityChange);
+            //PlayerRB.AddForce(new Vector3(0,0,0), ForceMode.VelocityChange); 
+            PlayerRB.Sleep();
             Debug.LogError("if you see this there is no god");
         }
         if (PlayerRB.velocity.magnitude > MaxSpeed)
