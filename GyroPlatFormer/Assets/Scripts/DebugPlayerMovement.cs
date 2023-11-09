b86bfcc7e3e59f8b3512b50386e76721faf41e14
AIE\s229025 <jrverburg@yahoo.com>
2023-11-08 16:57:18 -0800

    Added PC player controller

--- GyroPlatFormer/Assets/Scripts/DebugPlayerMovement.cs
+++ GyroPlatFormer/Assets/Scripts/DebugPlayerMovement.cs
@@ -4,5 +4,35 @@ using UnityEngine;
 
 public class DebugPlayerMovement : MonoBehaviour
 {
+    public GameObject Camera;
+    public float MoveSpeed;
+    public float JumpHeight;
+    public float MaxMoveSpeed;
+    public float AntiSlide;
+    public Rigidbody PlayerRB;
+    private bool Inair;
+    public float MouseSens;
 
+    public void Update()
+    {
+        if (PlayerRB.velocity.magnitude > MaxMoveSpeed)
+        {
+            PlayerRB.velocity = PlayerRB.velocity.normalized * MaxMoveSpeed;
+        }
+        if(Input.GetKey(KeyCode.W) == false && Input.GetKey(KeyCode.A) == false && Input.GetKey(KeyCode.S) == false && Input.GetKey(KeyCode.D) == false && Inair == false)
+        {
+            PlayerRB.AddForce(-PlayerRB.velocity * AntiSlide, ForceMode.Acceleration); 
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && Inair == false)
+        {
+            Inair = true;
+            PlayerRB.AddForce(transform.up * JumpHeight, ForceMode.VelocityChange);
+        }
+
+        PlayerRB.AddForce(transform.forward.normalized * Input.GetAxis("Vertical") * MoveSpeed,ForceMode.Acceleration);
+        PlayerRB.AddForce(transform.right.normalized * Input.GetAxis("Horizontal") * MoveSpeed, ForceMode.Acceleration);
+        Camera.transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), 0,0));
+        transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
+
+    }
 }
